7caf0a974461d252f257bba38d225604f8136fb1
martazapico <maartazapico@gmail.com>
2026-02-10 16:37:52 +0100

    Exercise 3: Counting DNA bases from a file. FIRST WAY

---
+++ S03/dna_count_file.py
@@ -0,0 +1,21 @@
+lines = ["AGTACACTGGT", "ACCAGTGTACT", "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"]
+
+f = open("dna.txt.", "r")
+lines = f.readlines()
+f.close()
+
+bases = {"A": 0, "C": 0, "T": 0, "G": 0}
+
+total = 0
+
+for sequence in lines:
+    sequence = sequence.strip()
+    total += len(sequence)
+    for i in sequence:
+        if i in bases:
+            bases[i]+=1
+
+print("Total number of bases:", total)
+for base, count in bases.items():
+    print(f'{base}: {count}')
+
