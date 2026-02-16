a5f882e3f479ed0fe76a5d61f18808b3b391cb02
martazapico <maartazapico@gmail.com>
2026-02-16 16:03:56 +0100

    Exercise 5: Loops

---
+++ Practical exercises/ex5_loops.py
@@ -0,0 +1,12 @@
+#5a: for loop:
+words = ["Python", "is", "a", "programming", "language"]
+
+for i in words:
+    result = f'{i} -> {len(i)} characters'
+    print(result)
+#5b: while loop:
+
+n = 1
+while n < 1024:
+    n = n * 2
+    print(n)
