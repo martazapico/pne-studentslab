bf168149f3767d292cce8a5b93c33340ac7cb3b2
martazapico <maartazapico@gmail.com>
2026-02-17 16:26:34 +0100

    Exercise 5

---
+++ S04/sequence.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+# -- Constant with the new of the file to open
+FILENAME = "sequences/ADA.txt"
+
+# -- Open and read the file
+file_contents = Path(FILENAME).read_text()
+
+a = file_contents.split("\n")
+body = a[1::]
+b = "".join(body)
+print(b)
+total_bases = len(b)
+print(total_bases)
