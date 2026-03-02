fb41f03ec3eb56cb0bc0854658bd7b7140a083e7
martazapico <m.zapico.2025@alumnos.urjc.es>
2026-03-02 18:17:32 +0100

    exercise 3

--- S06/e3.py
+++ S06/e3.py
@@ -1,6 +1,7 @@
 class Seq:
     def __init__(self, strbases):
         self.strbases = strbases
+        print("New sequence created!")
 
     def __str__(self):
         return self.strbases
@@ -8,7 +9,6 @@ class Seq:
     def len(self):
         return len(self.strbases)
 
-seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
 def print_seqs(seq_list):
     for i in seq_list:
         index = seq_list.index(i)
@@ -16,4 +16,22 @@ def print_seqs(seq_list):
         sequence = i
         print(f"Sequence {index}: (Length: {length}) {sequence}")
 
-print_seqs(seq_list)
+
+
+def generate_seqs(pattern, number):
+    list_a = []
+    for i in range(1, number + 1):
+        letter = pattern * i
+        list_a.append(Seq(letter))
+    return list_a
+
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
