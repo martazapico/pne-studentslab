4e9d687492c26d907048c577b13704c77d7e7e66
martazapico <maartazapico@gmail.com>
2026-03-03 16:35:17 +0100

    exercise 3

---
+++ S08/e3.py
@@ -0,0 +1,19 @@
+import socket
+PORT = 8081
+IP = "212.128.255.64" #
+
+while True:
+    # -- Ask the user for the message
+    message = input("Please, enter a message: ")
+    # -- Create the socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    # -- Establish the connection to the Server
+    s.connect((IP, PORT))
+
+
+    # -- Send the user message
+    s.send(str.encode(message))
+    # -- Close the socket
+    s.close()
