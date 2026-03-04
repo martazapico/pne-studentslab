4b6437dce49f32c8e9e065fcf0be106ebac7dfa2
martazapico <maartazapico@gmail.com>
2026-03-04 17:26:02 +0100

    practice 2

---
+++ P02/e2.py
@@ -0,0 +1,13 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 2
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+IP = "212.128.255.97" # your IP address
+PORT = 8081
+
+#...
+c = Client(IP, PORT)
+print(c)
+#...
