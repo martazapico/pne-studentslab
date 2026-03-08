864989faa7cdd2747b705b08d5f5c2f63b11030d
martazapico <m.zapico.2025@alumnos.urjc.es>
2026-03-08 13:31:15 +0100

    exercise 5, final solution

--- P02/e5.py
+++ P02/e5.py
@@ -1,3 +1,4 @@
+from P01.Seq1_new_version import Seq
 from Client0 import Client
 
 PRACTICE = 2
@@ -5,6 +6,7 @@ EXERCISE = 5
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 IP = "212.128.255.97" # your IP address
+
 PORT = 8081
 c = Client(IP, PORT)
 
@@ -17,8 +19,19 @@ gene = "FRAT1"
 
 FILENAME = f"../S04/sequences/{gene}.txt"
 gene_bases = s.read_fasta(FILENAME)
+print(f"Gene {gene}: {str(gene_bases)}")
+
 importing = c.talk(f"Sending {gene} Gene to the server, in fragments of 10 bases...")
-print(f"{importing}\n From client: Sending {gene} Gene to the server, n fragments of 10 bases...")
-send_bases = c.talk(str(gene_bases))
-print(f"\n{importing}\n To server:{str(gene_bases)}")
+
+
+
+number = 1
+for i in range(0, len(gene_bases), 10):
+    fragment = gene_bases[i:i + 10]
+    print(f"Fragment {number}: {fragment}")
+    c.talk(f"Fragment {number}: {fragment}")
+    number += 1
+    if number > 5:
+        break
+
 
