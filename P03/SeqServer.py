804b05234a15a47485dfa4ac6045090cb2f6f60b
martazapico <maartazapico@gmail.com>
2026-03-11 17:36:54 +0100

    exercise 1, completed

---
+++ P03/SeqServer.py
@@ -0,0 +1,49 @@
+import termcolor
+import socket
+
+
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+PORT = 8080
+IP = "127.0.0.1"
+
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+ls.bind((IP, PORT))
+ls.listen()
+
+print("SEQ Server configured!")
+count = 0
+
+while True:
+    count += 1
+    # -- Waits for a client to connect
+    print("Waiting for clients")
+    (cs, client_ip_port) = ls.accept()
+
+    # -- Read the message from the client
+    # -- The received message is in raw bytes
+    msg_raw = cs.recv(2048)
+
+    # -- We decode it for converting it
+    # -- into a human-redeable string
+    msg = msg_raw.decode()
+
+    if msg.strip() == "PING":
+    # -- Print the received message
+        termcolor.cprint("PING command!", 'green')
+
+        # -- Send a response message to the client
+        response = "OK!\n"
+        print("OK!\n")
+
+    # -- The message has to be encoded into bytes
+        cs.send(response.encode())
+
+
+    # -- Close the data socket
+    cs.close()
+
+
