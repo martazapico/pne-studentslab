1fe7aa38d04e591e622bf485554fad19c79c9a0e
martazapico <m.zapico.2025@alumnos.urjc.es>
2026-04-24 16:24:15 +0200

    Exercise 3. I had to alter exercise 2 in order to use it efficiently for this exercise.

--- P07/e2.py
+++ P07/e2.py
@@ -1,50 +1,58 @@
 import http.client
 import json
 import termcolor
+def get_genes():
+    genes = {}
+    species = "homo_sapiens"
+    gene_list = ["FRAT1", "ADA", "FXN", "RNU6-269P", "MIR633", "TTTY4C", "RBMY2YP", "FGFR3", "KDR", "ANK2"]
 
-genes = {}
-species = "homo_sapiens"
-gene_list = ["FRAT1", "ADA", "FXN", "RNU6-269P", "MIR633", "TTTY4C", "RBMY2YP", "FGFR3", "KDR", "ANK2"]
-num_genes = len(gene_list)
+    num_genes = len(gene_list)
 
-print(f"Dictionary of Genes!\nThere are {num_genes} genes in the dictionary:\n")
-for name in gene_list:
+    text1 = f"Dictionary of Genes!\nThere are {num_genes} genes in the dictionary:\n"
+    for name in gene_list:
 
-    SERVER = 'rest.ensembl.org'
-    ENDPOINT = f'/xrefs/symbol/{species}/{name}'
-    PARAMS = '?content-type=application/json'
-    URL = SERVER + ENDPOINT + PARAMS
-    REQUEST = ENDPOINT + PARAMS
+        SERVER = 'rest.ensembl.org'
+        ENDPOINT = f'/xrefs/symbol/{species}/{name}'
+        PARAMS = '?content-type=application/json'
+        URL = SERVER + ENDPOINT + PARAMS
+        REQUEST = ENDPOINT + PARAMS
 
 
-    # Connect with the server
-    conn = http.client.HTTPConnection(SERVER)
+        # Connect with the server
+        conn = http.client.HTTPConnection(SERVER)
 
-    # -- Send the request message, using the GET method. We are
-    # -- requesting the main page (/)
-    try:
-        conn.request("GET", REQUEST)
-    except ConnectionRefusedError:
-        print("ERROR! Cannot connect to the Server")
-        exit()
+        # -- Send the request message, using the GET method. We are
+        # -- requesting the main page (/)
+        try:
+            conn.request("GET", REQUEST)
+        except ConnectionRefusedError:
+            print("ERROR! Cannot connect to the Server")
+            exit()
 
-    # -- Read the response message from the server
-    r1 = conn.getresponse()
+        # -- Read the response message from the server
+        r1 = conn.getresponse()
 
-    # -- Read the response's body
-    data1 = r1.read().decode("utf-8")
+        # -- Read the response's body
+        data1 = r1.read().decode("utf-8")
 
-    # -- Create a variable with the data,
-    # -- form the JSON received
-    response = json.loads(data1)
+        # -- Create a variable with the data,
+        # -- form the JSON received
+        response = json.loads(data1)
 
-    for key, value in response[0].items():
-        if key == "id":
-            ID = value
-    genes[name] = ID
+        for key, value in response[0].items():
+            if key == "id":
+                ID = value
+        genes[name] = ID
+    return genes, text1
+if __name__ == "__main__":
+    genes, text1 = get_genes()
+    print(text1)
 
-for key, value in genes.items():
-    termcolor.cprint(f"{key}:", 'green', end='')
-    print(f" --> {value}")
+    for key, value in genes.items():
+        termcolor.cprint(f"{key}:", 'green', end='')
+        print(f" --> {value}")
 
+    gene_list = []
+    for key, value in genes.items():
+        gene_list.append(f"{key}:{value}")
 
---
+++ P07/e3.py
@@ -0,0 +1,49 @@
+import http.client
+import json
+import termcolor
+from e2 import get_genes
+genes, text1 = get_genes()
+
+gene = "MIR633"
+ID = genes[gene]
+
+SERVER = 'rest.ensembl.org'
+ENDPOINT = f'/sequence/id/{ID}'
+PARAMS = '?content-type=application/json'
+URL = SERVER + ENDPOINT + PARAMS
+REQUEST = ENDPOINT + PARAMS
+
+print()
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", REQUEST)
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# -- Create a variable with the data,
+# -- form the JSON received
+response = json.loads(data1)
+
+termcolor.cprint(f"Gene", 'green', end=" ")
+print(gene)
+termcolor.cprint(f"Description", 'green', end=" ")
+print(response["desc"])
+termcolor.cprint(f"Bases", 'green', end=" ")
+print(response["seq"])
